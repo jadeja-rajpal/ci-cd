40a6dd7878002852104674eab629dd0c8c60d3f4
Rajpal Jadeja <rhjadeja004@gmail.com>
2026-06-14 17:37:32 +0530

    checking synaatc

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,5 @@
 const express = require("express");
 const cors = require("cors");
-const API_KEY = "123456789SECRETKEY";
-const Access_key="cbskscuvd2qubkb"
 const app = express();
 
 app.use(cors());
